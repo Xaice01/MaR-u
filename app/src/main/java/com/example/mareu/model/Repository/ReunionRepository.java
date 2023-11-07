744df1afccf0753d12411c56053aef53ae82a6dd
Xaice01 <103635912+Xaice01@users.noreply.github.com>
2023-11-07 16:03:09 +0100

    Etape 03: Mise en place du patron de conception Repository pour la couche de données qui communique avec l’API

---
+++ app/src/main/java/com/example/mareu/model/Repository/ReunionRepository.java
@@ -0,0 +1,50 @@
+package com.example.mareu.model.Repository;
+
+
+import com.example.mareu.model.Reunion;
+import com.example.mareu.model.service.ReunionApiService;
+
+import java.util.List;
+
+/**
+ * Create a ReunionRepository
+ * <p>
+ * in Singleton
+ */
+public class ReunionRepository {
+    private static ReunionApiService service;
+
+
+    public ReunionRepository(ReunionApiService service) {
+        this.service = service;
+    }
+
+    public List<Reunion> getReunions() {
+        return service.getReunions();
+    }
+
+    public void createReunion(Reunion reunion) {
+        service.createReunion(reunion);
+    }
+
+    public void deleteReunion(Reunion reunion) {
+        service.deleteReunion(reunion);
+    }
+
+    private static volatile ReunionRepository instance;
+
+    /**
+     * Injection de dépendance (pour avoir une seule instance ReunionRepository)
+     *
+     * @param service
+     * @return instance_of_ReunionRepository
+     */
+    public static ReunionRepository getInstance(ReunionApiService service) {
+        if (instance == null) {
+            synchronized (ReunionRepository.class) {
+                instance = new ReunionRepository(service);
+            }
+        }
+        return instance;
+    }
+}
